47b272389ff2dddc230308808929dadd9071b70f
Yupin-Dasri <74158072+Yupin-Dasri@users.noreply.github.com>
2020-11-10 16:20:52 -0500

    added some sentences

--- src/LoginService.java
+++ src/LoginService.java
@@ -2,5 +2,6 @@ public class LoginService {
     public static void main(String[] args) {
         System.out.println("What would you like to eat");
         // I like to eat Thai food
+        //I also love Korean BBQ too
     }
 }
